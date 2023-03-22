fa7118385e7ae4fe8ef7c375e39bcf25a5352c83
Luis Sandoval <lsandovalancco@gmail.com>
2023-03-22 16:40:40 -0500

    Basic Auth: Configuración basica para la implementacion de autenticación básica

---
+++ src/main/java/com/lsandoval/springsecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.lsandoval.springsecurity.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// Este sera la clase para la configuracion de spring secutiry
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    // Sobreescrimos el metodo de configuracion de la clase WebSecurityConfigurerAdapter
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                // Todas las solicitudes
+                .anyRequest()
+                // Requieren autenticacion
+                .authenticated()
+                .and()
+                // Del tipo basic auth
+                .httpBasic();
+    }
+}
