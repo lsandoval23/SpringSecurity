648bfa5b836f180f9cc30f5088b1f2cbac2aa777
Luis Sandoval <lsandovalancco@gmail.com>
2023-04-12 15:28:13 -0500

    Se definen privilegios para los usuarios. Y se aumentan antMatchers para restringir endpoints y metodos HTTP en base a los permisos que se tengan (PERMISSION BASED AUTHENTICATION).

--- src/main/java/com/lsandoval/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/lsandoval/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.lsandoval.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.lsandoval.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.lsandoval.springsecurity.security.ApplicationUserRole.*;
 
 // Este sera la clase para la configuracion de spring secutiry
@@ -31,11 +33,21 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
+        http
+                // Deshabilitamos el token csrf para los metodos post y get ??
+                .csrf().disable()
+                .authorizeRequests()
                 // Metodo usado para permitir solicitudes a paths sin autenticacion
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 // aumentando el metodo hasRole a los antMatchers podemos restringir endpoints a ciertos roles.
                 .antMatchers("/api/**/").hasRole(STUDENT.name())
+                // Usamos el metodo hasAuthority para definir que permisos son los que tienen acceso a un endpoint en especifico
+                // usando antmatchers, tambien definiendo a que metodos HTTP se tiene acceso
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                // Aquellos que tengan el rol, admin o admin trainee tendran acceso al endpoint de management
+                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 // Todas las solicitudes
                 .anyRequest()
                 // Requieren autenticacion
@@ -57,18 +69,31 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())  // ROLE_STUDENT
+//                .roles(STUDENT.name())  // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name()) // ROLE_ADMIN
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                // Podemos agregar authorities, y dentro el rol
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+
+
 
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
-                lindaUser
+                lindaUser,
+                tomUser
         );
     }
 }
--- src/main/java/com/lsandoval/springsecurity/security/ApplicationUserRole.java
+++ src/main/java/com/lsandoval/springsecurity/security/ApplicationUserRole.java
@@ -1,25 +1,50 @@
 package com.lsandoval.springsecurity.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.lsandoval.springsecurity.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
 
+    // 4 tipos de objetos para los roles y sus permisos asociados
+
     STUDENT(Sets.newHashSet()),
     ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
     ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
 
+    // Atributps
     private final Set<ApplicationUserPermission> permissions;
 
+    // Constructor
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
     }
 
+    // Getter
     public Set<ApplicationUserPermission> getPermissions() {
         return permissions;
     }
+
+    // Transformamos los string del atributo permissions a un objeto de la clase SimpleGrantedAuthority.
+    // Ademas añadimos el role con el formato que usa la clase User (ROLE_)
+    // Esto se lo pasamos al builder authorites
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+
+        Set<SimpleGrantedAuthority> permissions = this.getPermissions()
+                                                        .stream()
+                                                        .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                                                        .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+
+        return permissions;
+
+    }
+
+
 }
